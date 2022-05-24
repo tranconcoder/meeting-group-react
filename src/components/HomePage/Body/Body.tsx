80e76ac09330a3f3c4cd43b2dbe3a00f704b57b2
tranconcoder <tranconcoder@gmail.com>
2022-05-24 22:36:04 +0700

    build imageSlideList

--- src/components/HomePage/Body/Body.tsx
+++ src/components/HomePage/Body/Body.tsx
@@ -2,6 +2,7 @@
 import { getClassNameModuleGenerator } from '../../../common/commonMethods';
 import Banner from './Banner/Banner';
 import styles from './Body.module.scss';
+import ImagesSlide from './ImagesSlide/ImagesSlide';
 import Users from './Users/Users';
 
 const cx = getClassNameModuleGenerator(styles);
@@ -12,6 +13,8 @@ function HomePageBody() {
 			<Banner />
 
 			<Users />
+
+			<ImagesSlide />
 		</div>
 	);
 }
---
+++ src/components/HomePage/Body/ImagesSlide/ImagesSlide.tsx
@@ -0,0 +1,82 @@
+//@ts-ignore
+import React, { useEffect, useRef, useState } from 'react';
+import { getClassNameModuleGenerator } from '../../../../common/commonMethods';
+import styles from './ImagesSlide.module.scss';
+import imageSlide1 from '../../../../images/images-slide (1).jpg';
+import imageSlide2 from '../../../../images/images-slide (2).jpg';
+import imageSlide3 from '../../../../images/images-slide (3).jpg';
+import imageSlide4 from '../../../../images/images-slide (4).jpg';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+
+const cx = getClassNameModuleGenerator(styles);
+
+function ImagesSlide() {
+	const imageList: string[] = [imageSlide1, imageSlide2, imageSlide3, imageSlide4];
+
+	const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+	const imageListRef = useRef<HTMLUListElement>(null);
+
+	const handleClickNextButton = () => {
+		setCurrentImageIndex(prev => (prev === imageList.length - 1 ? 0 : prev + 1));
+	};
+
+	const handleClickPrevButton = () => {
+		setCurrentImageIndex(prev => (prev === 0 ? imageList.length - 1 : prev - 1));
+	};
+
+	// Set scroll after rendered
+	useEffect(() => {
+		const imageListElm = imageListRef.current as HTMLUListElement;
+		const scrollStep = imageListElm.clientWidth;
+
+		console.log(currentImageIndex);
+
+		imageListElm.scrollLeft = scrollStep * currentImageIndex;
+	}, [currentImageIndex]);
+
+	// Auto next slide
+	useEffect(() => {
+		const timeoutId = setTimeout(() => {
+			handleClickNextButton();
+		}, 10000);
+
+		return () => {
+			clearTimeout(timeoutId);
+		};
+	}, [currentImageIndex]);
+
+	return (
+		<div data-aos="fade-up" className={cx('images-slide-container')}>
+			<button className={cx('prev-button')} onClick={handleClickPrevButton}>
+				<FaChevronLeft />
+			</button>
+
+			{/* Image list */}
+			<ul className={cx('image-list')} ref={imageListRef} onScroll={e => e.preventDefault()}>
+				{imageList.map((image, index) => (
+					<li key={index}>
+						<img src={image} alt="" />
+					</li>
+				))}
+			</ul>
+
+			{/* Progress dot list */}
+			<ul className={cx('progress-dot-list')}>
+				{imageList.map((image, index) => (
+					<li
+						key={index}
+						data-active={currentImageIndex === index ? 'true' : 'false'}
+						onClick={() => setCurrentImageIndex(index)}
+					></li>
+				))}
+			</ul>
+
+			<button className={cx('next-button')} onClick={handleClickNextButton}>
+				<FaChevronRight />
+			</button>
+		</div>
+	);
+}
+
+export default ImagesSlide;
